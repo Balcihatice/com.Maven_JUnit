a9cf60f5bdd4a5679832056e54a32d486daaae96
Balcihatice <aciceaybike@mail.com>
2023-01-15 23:27:05 +0300

    Selenium Notlari

--- src/test/java/day16/C01_ExcelRead.java
+++ src/test/java/day16/C01_ExcelRead.java
@@ -1,16 +1,19 @@
 package day16;
 
 import org.apache.poi.ss.usermodel.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 
 public class C01_ExcelRead {
     @Test
 
-    public void readExcelTest() throws IOException {
+    public void readExcelTest() throws Exception {
         //Java dosyasina sag tikla ve ressources adinda bir klasor olustur
         //Excel dosyasini bu klasore at
         //resources dosyasi excel ,word, text... gibi data dostalarinin oldugu klasor klasorun adidir.
@@ -43,7 +46,39 @@ public class C01_ExcelRead {
         System.out.println(cell1);
 
         //1.SATIR 2. SUTUN
-        Cell cell2 = sheet1.getRow(0).getCell(1);
-        System.out.println(cell2);
+        Cell cell12 = sheet1.getRow(0).getCell(1);
+        System.out.println(cell12);
+
+        //3.satir 1.sutun yazdir ve O verirnin France oldugunu test et
+        String cell31 = sheet1.getRow(2).getCell(0).toString();
+        System.out.println(cell31);
+        Assert.assertEquals("France", cell31);
+
+        //Exceldeki toplam satir sayisini bul
+        int toplamSatirSayisi = sheet1.getLastRowNum()+1;//son satir numarasi.index sifirdan basliyor,saymasayilari 1 den,o yuzden +1 ekledik
+        System.out.println(toplamSatirSayisi);
+
+        //Kullanilan toplam satir sayisi bul(bazi hucreler bos ise)
+        int kullanilanToplamSatirSayisi = sheet1.getPhysicalNumberOfRows();
+        System.out.println(kullanilanToplamSatirSayisi);//1 den basliyor
+
+        //COUNTRY, CAPITALS key ve valulari map a alip print et
+        //  {{USA,D,C},{FRANCE,PARIS},...}
+        // Variable olusturalim. Bu variable exceldeki countr, capital verilerini tutacak
+
+        Map<String,String> ulkeBaskentleri = new HashMap<>();
+
+        for (int satirSayisi=1; satirSayisi<kullanilanToplamSatirSayisi; satirSayisi++){
+
+            String country = sheet1.getRow(satirSayisi).getCell(0).toString().toString();
+            String capital = sheet1.getRow(satirSayisi).getCell(1).toString();
+            ulkeBaskentleri.put(country,capital);
+
+        }
+        System.out.println(ulkeBaskentleri);
+
+
     }
+
+
 }
