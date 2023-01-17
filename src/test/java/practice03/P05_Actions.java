963d7240f66dd16e6b53e4acd2a7834a6e23d213
Balcihatice <aciceaybike@mail.com>
2023-01-17 21:28:45 +0300

    Practice

--- src/test/java/practice03/P05_Actions.java
+++ src/test/java/practice03/P05_Actions.java
@@ -1,6 +1,8 @@
 package practice03;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
@@ -12,20 +14,27 @@ public class P05_Actions extends TestBase {
         //- http://szimek.github.io/signature_pad/ sayfasına gidiniz
         driver.get("http://szimek.github.io/signature_pad/");
 
-
         //- Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
-        Actions actions = new Actions(driver);
-        actions.clickAndHold().perform();
-        for (int i = 0; i <10 ; i++) {
+        WebElement canvas = driver.findElement(By.xpath("//canvas"));
 
+        Actions actions = new Actions(driver);
+        actions.moveToElement(canvas).clickAndHold();
+
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(-5, -5);
         }
 
-
-
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(0, 5);
+        }
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(5, 0);
+        }
+        actions.release().build().perform();
+        waitFor(5);
 
         //- Son olarak sayfayı temizleyiniz
+        driver.findElement(By.xpath("//*[text()='Clear']")).click();
 
-
-        //
     }
 }
